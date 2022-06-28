06a0a3bb6f83ccbb4d8caaac9f1b3171e4e5a690
Maxime Preaux <hello@maxime.dev>
2022-06-28 14:24:51 -0700

    CLI script for uploading commands

--- src/setup-commands.ts
+++ src/setup-commands.ts
@@ -1,8 +1,28 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
+const yargs = require("yargs");
 
-import "dotenv/config";
+const options = yargs
+  .usage("Usage: -t <token> -a <application_id> -g <guild_id>")
+  .option("t", {
+    alias: "token",
+    describe: "Discord bot token",
+    type: "string",
+    demandOption: true,
+  })
+  .option("a", {
+    alias: "application_id",
+    describe: "Discord application/client id",
+    type: "string",
+    demandOption: true,
+  })
+  .option("g", {
+    alias: "guild_id",
+    describe: "Discord guild/server id",
+    type: "string",
+    demandOption: true,
+  }).argv;
 
 // Register the slash commands available to users
 const commands = [
@@ -13,16 +33,15 @@ const commands = [
     ),
 ].map((command) => command.toJSON());
 
-const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_BOT_TOKEN);
+const rest = new REST({ version: "9" }).setToken(options.token);
 
 // Upload the commands to the Discord server
 rest
   .put(
-    Routes.applicationGuildCommands(
-      process.env.DISCORD_CLIENT_ID,
-      process.env.DISCORD_GUILD_ID
-    ),
-    { body: commands }
+    Routes.applicationGuildCommands(options.application_id, options.guild_id),
+    {
+      body: commands,
+    }
   )
   .then(() => console.log("Successfully registered application commands."))
   .catch(console.error);
